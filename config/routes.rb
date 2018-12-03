ac254a61ecff629df2aa41e23722714a5fb2aaa2
Dharmesh Tarapore <dharmesh@bu.edu>
2018-12-02 23:16:36 -0500

    Autopush.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'logins#snew'
 
   get '/login', to: 'logins#snew'
   post '/login', to: 'logins#screate'
